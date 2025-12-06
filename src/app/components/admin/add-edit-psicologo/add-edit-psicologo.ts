c00546f4c623567d70fb68383a162cd0967e767c
LuisE1802 <u201918169@upc.edu.pe>
2025-12-06 01:49:28 -0500

    Update add-edit-psicologo.ts

--- src/app/components/admin/add-edit-psicologo/add-edit-psicologo.ts
+++ src/app/components/admin/add-edit-psicologo/add-edit-psicologo.ts
@@ -5,7 +5,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { AdminService } from '../../../services/admin.service';
 import { ADMINPsicologoDTO } from '../../../models/admin-psicologo.dto';
 
-
 @Component({
   selector: 'app-add-edit-psicologo',
   standalone: false,
