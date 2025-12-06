deee38f834da9111d6071141538f2e4235c042d8
JoseGranaMed <jgranadosm11@gmail.com>
2025-12-06 01:29:33 -0500

    Update material-module.ts

--- src/app/modules/material/material-module.ts
+++ src/app/modules/material/material-module.ts
@@ -17,6 +17,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+
+
 @NgModule({
   declarations: [],
   imports: [
